c5204cb247d17d2b8aaa40fb2a209455eb0e36cd
ryanppei <ryanppei@gmail.com>
2021-10-04 16:23:20 +0000

    fix

--- schema/Orders.js
+++ schema/Orders.js
@@ -2,9 +2,7 @@ cube(`Orders`, {
   sql: `SELECT * FROM public.orders`,
   preAggregations: {// Pre-Aggregations definitions go here
     // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
-    }
-    
-  },
+    },
   joins: {},
   measures: {
     count: {
